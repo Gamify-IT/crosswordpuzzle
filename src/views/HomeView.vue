d03f58e09e64b19159a40f9fcbf79f9f71cfc92b
AaronSchimd <st166450@stud.uni-stuttgart.de>
2022-05-05 19:26:36 +0200

    initial alogrithm

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 import TheWelcome from "@/components/TheWelcome.vue";
+import { generateCrossword } from "@/crosswordgenerator";
+import type { question } from "@/types/index";
+import type { tileCrossWord} from "@/types/index";
+
+let quest1 = {
+  answer: "Syntax",
+  question: "Testquestion1"
+};
+let quest2 = {
+  answer: "for-schleife",
+  question: "Testquestion2"
+};
+let quest3 = {
+  answer: "typescript",
+  question: "Testquestion3"
+};
+let questions = [quest1,quest2,quest3];
+generateCrossword(questions);
 </script>
 
 <template>
