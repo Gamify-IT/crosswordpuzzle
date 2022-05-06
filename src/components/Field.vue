8d6841a32033b9438ba78f80b843e05fbe2a5fd7
madebyTimo <t.schnaible@madebytimo.de>
2022-05-06 16:34:31 +0200

    create basic elements

---
+++ src/components/Field.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  crosswordTile: string;
+}>();
+</script>
+
+<template>
+  <div class="field">
+    <input class="form-control" type="text">
+  </div>
+</template>
+
+<style scoped>
+
+  .field {
+    width: 2em;
+    height: 2em;
+  }
+
+</style>
--- src/views/Crosswordpuzzle.vue
+++ src/views/Crosswordpuzzle.vue
@@ -1,10 +1,43 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Field from "@/components/Field.vue";
+
+interface tileCrossWord {
+    answer: string;
+    currentLetter: string;
+    startPoint: boolean;
+    startDirection: string;
+}
+
+export default defineComponent({
+    name: "Crosswordpuzzle",
+    components: {
+        Field
+    },
+    props: {
+        crosswordpuzzle: Array
+    
+  }
+});
+
+
+
+
 </script>
 
 <template>
-  
+
+<div class="container">
+  <div class="row row-cols-auto" v-for="crosswordRow in crosswordpuzzle">
+    <div class="col" v-for="crosswordTile in crosswordRow">
+        <Field crosswordTile="crosswordTile" />
+    </div>
+  </div>
+</div>
+
 </template>
 
-<style>
 
+
+<style>
 </style>
