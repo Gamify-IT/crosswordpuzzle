231a08670be9077545aaab07e30da9e107c595f9
madebyTimo <t.schnaible@madebytimo.de>
2022-05-09 00:12:07 +0200

    resize and redesign Question list

--- src/views/Crosswordpuzzle.vue
+++ src/views/Crosswordpuzzle.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import type { tileCrossWord } from "@/types/index";
+import type { question } from "@/types/index"
 
 defineProps<{
   crosswordpuzzle: tileCrossWord[][];
+  questions: question[];
 }>();
 </script>
 
@@ -23,11 +25,24 @@ export default defineComponent({
 </script>
 
 <template>
-
-  <div class="container my-4">
-    <div class="row row-cols-auto m-0 p-0" v-for="crosswordRow in crosswordpuzzle">
-      <div class="col m-0 p-0" v-for="crosswordTile in crosswordRow">
-        <Field :crosswordTile="crosswordTile" />
+  <div class="container">
+    <div class="row">
+      <div class="col-9">
+        <div class="container my-4">
+          <div class="row row-cols-auto m-0 p-0" v-for="crosswordRow in crosswordpuzzle">
+            <div class="col m-0 p-0" v-for="crosswordTile in crosswordRow">
+              <Field :crosswordTile="crosswordTile" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <ol class="list-group">
+          <h1>Fragen</h1>
+        </ol>
+        <ol v-for="question in questions" class="list-group list-group-numbered">
+          {{ question.question }}
+        </ol>
       </div>
     </div>
   </div>
