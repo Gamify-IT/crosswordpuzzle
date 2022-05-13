5ec3209800762041f44aafbd5e805c78063ca665
madebyTimo <t.schnaible@madebytimo.de>
2022-05-13 17:59:12 +0200

    add new arrows, fix question number

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -11,27 +11,19 @@ defineProps<{
 <template>
   <div class="field">
     <div v-if="crosswordTile.startPoint">
-      <span
-        class="badge rounded-pill bg-dark position-absolute top-50 start-50 translate-middle"
-        >1</span
-      >
+      <span class="badge rounded-pill bg-dark position-absolute top-50 start-50 translate-middle">
+        {{ crosswordTile.answer }}
+      </span>
 
-      <div
-        v-if="crosswordTile.startDirection == 'right'"
-        class="position-absolute top-50 start-100 translate-middle"
-      >
-        <div class="arrow arrowRight"></div>
-      </div>
+      <img v-if="crosswordTile.startDirection == 'right'" src="@/assets/caret-right.svg"
+        class="arrowRight"/>
 
-      <div
-        v-if="crosswordTile.startDirection == 'down'"
-        class="position-absolute top-100 start-50 translate-middle"
-      >
-        <div class="arrow arrowDown"></div>
-      </div>
+      <img v-else-if="crosswordTile.startDirection == 'down'" src="@/assets/caret-right.svg"
+        class="arrowDown"/>
+
     </div>
 
-    <input v-if="crosswordTile.currentLetter != emptyTileString" class="form-control text-center" type="text"
+    <input v-else-if="crosswordTile.currentLetter != emptyTileString" class="form-control text-center" type="text"
       maxlength="1" :placeholder="crosswordTile.answer" v-model="crosswordTile.currentLetter">
   </div>
 </template>
@@ -41,24 +33,21 @@ defineProps<{
   width: 45px;
   height: 45px;
   position: relative;
-  text-align: center;
-}
-
-.arrow {
-  border: solid black;
-  border-width: 0 3px 3px 0;
-  display: inline-block;
-  padding: 3px;
 }
 
 .arrowRight {
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
+  translate: 0 -50%;
+  position: absolute;
+  top: 50%;
+  left: 70%;
 }
 
 .arrowDown {
-  transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
+  translate: -50% 0;
+  transform: rotate(90deg);
+  position: absolute;
+  top: 70%;
+  left: 50%;
 }
 
 input {
